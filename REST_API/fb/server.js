2c88daa9a021bd6fdb7a3e54698b022640c80aae
Fuad Emon <fuad.emon@springrain.io>
2023-05-08 12:23:28 +0600

    Rest api

---
+++ REST_API/fb/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+
+// init environment variable
+const PORT = process.env.PORT || 8080;
+
+// express init
+const app = express();
+
+// express middlewares
+app.use(express.json());
+app.use(express.urlencoded( { extended : false } ));
+
+// listen port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.bgBlue)
+});
